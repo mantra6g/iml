64951977130b7aafde10d7d12ae66b80b590612b
tomasagata <tomasagata00@gmail.com>
2025-11-26 20:44:15 +0100

    feat: updated ml-example

--- ml-example/serve_bw.py
+++ ml-example/serve_bw.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import mlflow
 from mlflow.tracking import MlflowClient
 import ray
@@ -8,7 +9,7 @@ MLFLOW_TRACKING_URI = "http://fns01.lab.uvalight.net:5000"
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
 # Model registry parameters
-MODEL_NAME = "iris_model"
+MODEL_NAME = "bandwidth_forecasting_model"
 MODEL_STAGE = "Production" # or specify a version manually
 
 def load_model_from_registry():
@@ -21,17 +22,19 @@ def load_model_from_registry():
     model_uri = f"models:/{MODEL_NAME}/{mv.version}"
 
     print(f"Loading model from MLflow Registry: {model_uri}")
-    model = mlflow.sklearn.load_model(model_uri)
+    model = mlflow.pyfunc.load_model(model_uri)
     return model
 
 
 @serve.deployment(num_replicas=2, max_replicas_per_node=1)
-class IrisModel:
+class BandwidthForecastingModel:
     def __init__(self):
         self.model = load_model_from_registry()
 
     async def __call__(self, request):
         data = await request.json()
-        return self.model.predict([data]).tolist()
+        df = pd.DataFrame(data)
+        forecast = self.model.predict(df)
+        return forecast.to_dict(orient="records")
 
-app = IrisModel.bind()
+app = BandwidthForecastingModel.bind()
--- ml-example/train.py
+++ ml-example/train.py
@@ -2,6 +2,7 @@ import mlflow
 import ray
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 import joblib
 
 # Point MLflow to the internal MLflow server on Kubernetes
@@ -11,14 +12,14 @@ mlflow.set_experiment("iris-training")
 
 def train_model():
 	data = load_iris()
-	X, y = data.data, data.target
+	X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
 
 	with mlflow.start_run():
 		model = RandomForestClassifier(n_estimators=150)
-		model.fit(X, y)
-
+		model.fit(X_train, y_train)
+		
 		# Log metric
-		acc = model.score(X, y)
+		acc = model.score(X_train, y_train)
 		mlflow.log_metric("train_accuracy", acc)
 
 		# Log artifact
