6e4fe28ab70a69dfc2e870383babeeddad4b0fb1
tomasagata <tomasagata00@gmail.com>
2025-12-15 22:18:25 +0100

    Removed nfvo directory and old examples

--- nfvo/blueprints/__init__.py
+++
@@ -1,9 +0,0 @@
-from apps_bp import apps_bp
-from chains_bp import service_chains_bp
-from vnfs_bp import network_functions_bp
-
-blueprints = [
-	apps_bp,
-	service_chains_bp,
-	network_functions_bp
-]
--- nfvo/nfvo.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from nfvo.blueprints import blueprints
-
-app = Flask(__name__)
-
-for blueprint in blueprints:
-	app.register_blueprint(blueprint)
-
