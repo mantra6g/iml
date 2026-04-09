39db8168ac69c8d2604c0984b44be42e649fba82
tomasagata <tomasagata00@gmail.com>
2026-04-09 07:47:02 -0300

    Removed unused directories and charts

--- install.sh
+++
@@ -1,5 +0,0 @@
-kubectl apply -f builder/dist/install.yaml
-kubectl apply -f cni/install.yml
-kubectl apply -f go-daemon/install.yml
-kubectl apply -f runtime-proxy/install.yml
-kubectl apply -f iml-oakestra-agent/install.yml
